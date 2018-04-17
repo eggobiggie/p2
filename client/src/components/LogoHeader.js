431853e58df688bdbf2e2955592e057fde7a02bd
Mia <miacrandell@gmail.com>
2018-04-17 12:20:37 -0600

    add logo image

--- client/src/components/LogoHeader.js
+++ client/src/components/LogoHeader.js
@@ -1,10 +1,14 @@
 import React from "react";
 import "./About.css";
-import Logo from "./images/logo1.png";
+import Logo from "./images/logo3.png";
 
 export const LogoHeader = props => (
-    <div className="container">
-        <img src={Logo} alt="Money Honey Logo" className="logo brand-logo center"></img>
+    <div className="row">
+    {/* <div className="col s12"> */}
+        <div className="logo-container center">
+            <img src={Logo} alt="Mia Crandell Logo" className="logo"></img>
+        </div>
+        {/* </div> */}
     </div>
 );
 
