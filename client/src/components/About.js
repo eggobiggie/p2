6c2a7316a44480a2c187129a4345a3e524067cf0
Mia <miacrandell@gmail.com>
2018-04-17 12:05:38 -0600

    initial server setup with client folder

---
+++ client/src/components/About.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./About.css";
+
+export const About = props => (
+    <div className="container">
+        <h1>hi</h1>
+    </div>
+);
---
+++ client/src/components/LogoHeader.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./About.css";
+import Logo from "./images/logo1.png";
+
+export const LogoHeader = props => (
+    <div className="container">
+        <img src={Logo} alt="Money Honey Logo" className="logo brand-logo center"></img>
+    </div>
+);
+
