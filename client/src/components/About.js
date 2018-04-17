9a0a6dfab4fb1ad42f21504449b76983b93df3a7
Mia <miacrandell@gmail.com>
2018-04-17 12:34:23 -0600

    begin About section

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,8 +1,15 @@
 import React from "react";
 import "./About.css";
+import Mia from "./images/mia.jpg";
 
 export const About = props => (
-    <div className="container">
-        <h1>hi</h1>
+    <div className="about-container">
+        <div className="row">
+            <div className="col s6">
+                <div className="center">
+                    <img src={Mia} alt="Mia's Face'" className="mia-face"></img>
+                </div>
+            </div>
+        </div>
     </div>
 );
--- client/src/components/LogoHeader.js
+++ client/src/components/LogoHeader.js
@@ -4,11 +4,9 @@ import Logo from "./images/logo3.png";
 
 export const LogoHeader = props => (
     <div className="row">
-    {/* <div className="col s12"> */}
-        <div className="logo-container center">
+        <div className="logo-container center z-depth-2">
             <img src={Logo} alt="Mia Crandell Logo" className="logo"></img>
         </div>
-        {/* </div> */}
     </div>
 );
 
