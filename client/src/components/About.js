5d2d48b52f50eadbe8458bd0a78887b1a411293f
Mia <miacrandell@gmail.com>
2018-04-17 14:28:26 -0600

    about section

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,14 +1,32 @@
 import React from "react";
 import "./About.css";
-import Mia from "./images/mia.jpg";
+import Mia from "./images/mia2.jpg";
+import Hello from "./images/hello4.png";
 
 export const About = props => (
     <div className="about-container">
-        <div className="row">
-            <div className="col s6">
-                <div className="center">
-                    <img src={Mia} alt="Mia's Face'" className="mia-face"></img>
+        <div className="yellow-container">
+            <div className="row">
+                <div className="col s12">
                 </div>
+            </div>
+        </div>
+        <div className="teal-container">
+            <div className="row">
+                <div className="col m3 offset-m1 s12">
+                    <div className="right">
+                        <img src={Mia} alt="Mia's Face'" className="mia-face z-depth-2"></img>
+                    </div>
+                </div>
+                <div className="col m4 s12">
+                <img src={Hello} alt="Hello Voice Bubble" className="hello-text"></img>
+                </div>
+                <div className="col s6">
+                <p className="description">I strive to bring fun, creativity, and hard work into everything I do. I studied animation at the Savannah College of Art and Design, and I recieved my BFA in 2010. I have since included my art background into a variety of environments. Now that I have discovered coding, I spend much of my time finding ways to mix both code and art into anything I can. I live in Denver with my three cats where I draw fun pictures, create embroidery, and, of course, code on a regular basis.</p>
+                </div>
+            </div>
+            <div className="row">
+                
             </div>
         </div>
     </div>
