5e4119bc9fad8bbbdf8bd2f8c3927e79bd1438d0
Mia <miacrandell@gmail.com>
2018-04-17 19:02:50 -0600

    add info to each card

--- client/src/components/PortfolioCards.js
+++ client/src/components/PortfolioCards.js
@@ -13,36 +13,36 @@ export const PortfolioCards = props => (
             <div className="col s4">
                 <Card className='small'
                     header={<CardTitle image={ Train }></CardTitle>}
-                    actions={[<a href='#'>This is a Link</a>]}>
-                    I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.
+                    actions={[<a href='https://github.com/eggobiggie/train-scheduler' target="_blank">Github</a>]}>
+                    Train Scheduler uses firebase to save train data and javascript to calculate train arrival times.
                 </Card>
             </div>
             <div className="col s4">
                 <Card className='small'
                     header={<CardTitle image={ Giftastic }></CardTitle>}
-                    actions={[<a href='#'>This is a Link</a>]}>
-                    I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.
+                    actions={[<a href='https://github.com/eggobiggie/giftastic' target="_blank">Github</a>]}>
+                    Giftastic allows users to click on buttons to generate themed gifs from the giphy API. Users can also add their own buttons
                 </Card>
             </div>
             <div className="col s4">
                 <Card className='small'
                     header={<CardTitle image={ Burger }></CardTitle>}
-                    actions={[<a href='#'>This is a Link</a>]}>
-                    I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.
+                    actions={[<a href='https://github.com/eggobiggie/its-burger-time' target="_blank">Github</a>]}>
+                    It's Burger Time is an app that utilizes Node.js and Express along with a mySQL database to save and update different types of burgers
                 </Card>
             </div>
             <div className="col s4">
                 <Card className='small'
                     header={<CardTitle image={ Cryptid }></CardTitle>}
-                    actions={[<a href='#'>This is a Link</a>]}>
-                    I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.
+                    actions={[<a href='https://github.com/eggobiggie/cryptid-collaborator' target="_blank">Github</a>]}>
+                    Cryptid Collaborator surveys users to match them with a cryptid by using Node.js and Express.
                 </Card>
             </div>
             <div className="col s4">
                 <Card className='small'
                     header={<CardTitle image={ Hangman }></CardTitle>}
-                    actions={[<a href='#'>This is a Link</a>]}>
-                    I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.
+                    actions={[<a href='https://github.com/eggobiggie/Hangman-Game' target="_blank">Github</a>]}>
+                    A JavaScript based hangman game with a Bob's Burgers theme
                 </Card>
             </div>
         </div>
