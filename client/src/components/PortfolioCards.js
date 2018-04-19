c7ebb8236d6c87566b4ce105846d016e8e885e6a
Mia <miacrandell@gmail.com>
2018-04-19 14:56:10 -0600

    adjust card sizes

--- client/src/components/PortfolioCards.js
+++ client/src/components/PortfolioCards.js
@@ -20,42 +20,42 @@ export const PortfolioCards = props => (
         </div>
         <div className="row">
             <div className="col m4 s12">
-                <Card className='small'
+                <Card className='medium'
                     header={<CardTitle image={ Train }></CardTitle>}
                     actions={[<a href='https://github.com/eggobiggie/train-scheduler' target="_blank" rel="noopener noreferrer">Github</a>]}>
                     <p className="card-description">Train Scheduler uses firebase to save train data and javascript to calculate train arrival times.</p>
                 </Card>
             </div>
             <div className="col m4 s12">
-                <Card className='small'
+                <Card className='medium'
                     header={<CardTitle image={ Giftastic }></CardTitle>}
                     actions={[<a href='https://github.com/eggobiggie/giftastic' target="_blank" rel="noopener noreferrer">Github</a>]}>
                     Giftastic allows users to click on buttons to generate themed gifs from the giphy API. Users can also add their own buttons
                 </Card>
             </div>
             <div className="col m4 s12">
-                <Card className='small'
+                <Card className='medium'
                     header={<CardTitle image={ Burger }></CardTitle>}
                     actions={[<a href='https://github.com/eggobiggie/its-burger-time' target="_blank" rel="noopener noreferrer">Github</a>]}>
                     It's Burger Time is an app that utilizes Node.js and Express along with a mySQL database to save and update different types of burgers
                 </Card>
             </div>
             <div className="col m4 s12">
-                <Card className='small'
+                <Card className='medium'
                     header={<CardTitle image={ Cryptid }></CardTitle>}
                     actions={[<a href='https://github.com/eggobiggie/cryptid-collaborator' target="_blank" rel="noopener noreferrer">Github</a>]}>
                     Cryptid Collaborator surveys users to match them with a cryptid by using Node.js and Express.
                 </Card>
             </div>
             <div className="col m4 s12">
-                <Card className='small'
+                <Card className='medium'
                     header={<CardTitle image={ Hangman }></CardTitle>}
                     actions={[<a href='https://github.com/eggobiggie/Hangman-Game' target="_blank" rel="noopener noreferrer">Github</a>]}>
                     A JavaScript based hangman game with a Bob's Burgers theme
                 </Card>
             </div>
             <div className="col m4 s12">
-                <Card className='small'
+                <Card className='medium'
                     header={<CardTitle image={ MoneyHoney }></CardTitle>}
                     actions={[<a href='https://github.com/chaucees/moneyhoney' target="_blank" rel="noopener noreferrer">Github</a>]}>
                     A full stack financial app that allows users to keep track of budgets
